66283c54bac6e62157e74dede8525da10d4f788b
Shivshankar Patil <patilshivshankar99@gmail.com>
2024-05-02 18:42:58 +0530

    proper comments were added

--- QuizGenerator/src/com/app/shivshankar/quiz/Question.java
+++ QuizGenerator/src/com/app/shivshankar/quiz/Question.java
@@ -3,19 +3,23 @@ package com.app.shivshankar.quiz;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//here Question class is created so that it can take questions and their options and and store correct option
 public class Question {
-	private String question;
-	private ArrayList<String> options;
-	private int correctOptionIndex;
+	private String question;	//used to store question
+	private ArrayList<String> options;	//used to store options
+	private int correctOptionIndex;	//used to store correct option
 	
+	//parameterized constructor
 	public Question(String que, ArrayList<String> optionList,int corrIndex) {
 		this.question=que;
 		this.options=optionList;
 		this.correctOptionIndex=corrIndex;
 	}
+	//parameterless constructor
 	public Question() {
 		options=new ArrayList<String>();
 	}
+	//addQuestion: this method is used to add questions and options with correct options
 	public void addQuestion()
 	{
 		Scanner sc=new Scanner(System.in);
@@ -30,14 +34,16 @@ public class Question {
 		correctOptionIndex=sc.nextInt();
 	}
 	
+	//this isCorrect method used to check your selected option and correct option are correct or not
 	public boolean isCorrect(int selectedOptionIndex) {
         return selectedOptionIndex == correctOptionIndex;
     }
 
+	//this displayQuestion method used to display questions one by one
     public void displayQuestion() {
         System.out.println(question);
         for (int i = 0; i < options.size(); i++) {
-            System.out.println((i + 1) + ". " + options.get(i));
+            System.out.println((i + 1) + ". " +options.get(i));
         }
     }
 }
--- QuizGenerator/src/com/app/shivshankar/quiz/Quiz.java
+++ QuizGenerator/src/com/app/shivshankar/quiz/Quiz.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
+//here class is created for main method
 public class Quiz {
 	
+	//method menu is written here so that you call multiple times in application
 	public static int menu()
 	{
 		Scanner sc=new Scanner(System.in);
@@ -20,8 +22,8 @@ public class Quiz {
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Question> gkquestions=new ArrayList<Question>();
-		HashMap<String,Integer> students=new HashMap<String,Integer>();
+		ArrayList<Question> gkquestions=new ArrayList<Question>();	//ArrayList used to store questions of quiz
+		HashMap<String,Integer> students=new HashMap<String,Integer>();	//Used to store students info who are giving exam
 		int score=0;
 		String name=null;
 		Scanner sc=new Scanner(System.in);
@@ -44,7 +46,7 @@ public class Quiz {
 				break;
 			case 2: System.out.println("Do you want to give quiz? (yes/no)");
 					String chooseoption=sc.next();
-					if(chooseoption.startsWith("y")) {
+					if(chooseoption.startsWith("y")) {	//if user enter string start with y then this method will work
 					System.out.println("Enter your name");
 					name=sc.next();
 					System.out.println("gkquestions.size(): "+gkquestions.size());
@@ -59,7 +61,8 @@ public class Quiz {
 					students.put(name, score);
 					}
 				break;
-			case 3:	for(Entry<String, Integer> entry:students.entrySet())
+			case 3:	for(Entry<String, Integer> entry:students.entrySet())	//here we have iterated students and their scores
+																			//Entry and entry set used to extract values from hashmap as key value pair									
 					System.out.println("Name: "+entry.getKey()+" Score: "+entry.getValue());
 				break;
 			default: System.out.println("You have entered wrong choice");
